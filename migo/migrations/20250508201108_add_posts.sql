f01b0051654b9c198f66499b8e1d32cd6d2192d7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-05-08 20:17:31 -0300

    feat: add migo and migration for posts

---
+++ migo/migrations/20250508201108_add_posts.sql
@@ -0,0 +1,12 @@
+-- Migration: add_posts
+-- Created at: 20250508201108
+
+-- UP
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+-- DOWN
